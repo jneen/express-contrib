2739d98a5b1bdf836d3c6ee5d54ef7d426e52bc3
Tj Holowaychuk <tj@vision-media.ca>
2010-09-06 10:52:19 -0700

    Added express-messages

--- test/messages.test.js
+++ test/messages.test.js
@@ -5,7 +5,10 @@
 
 var express = require('express'),
     messages = require('express-messages'),
-    contrib = require('express-contrib');
+    contrib = require('express-contrib'),
+    MemoryStore = require('connect/middleware/session/memory');
+
+var store = new MemoryStore({ reapInterval: -1 });
 
 module.exports = {
     'test contrib export': function(assert){
@@ -13,10 +16,23 @@ module.exports = {
     },
 
     'test messages dynamic helper': function(assert){
-        var app = express.createServer();
+        var app = express.createServer(
+            express.cookieDecoder(),
+            express.session({ store: store })
+        );
+        app.set('views', __dirname + '/fixtures');
         app.dynamicHelpers({ messages: messages });
         
         app.get('/', function(req, res, next){
+            req.flash('info', 'info one');
+            req.flash('info', 'info two');
+            req.flash('error', 'error one');
+            res.render('messages.ejs', {
+                layout: false
+            });
+        });
+        
+        app.get('/none', function(req, res, next){
             res.render('messages.ejs', {
                 layout: false
             });
@@ -32,10 +48,13 @@ module.exports = {
             '    <li>error one</li>',
             '  </ul>',
             '</div>'
-        ].join('');
+        ].join('\n');
 
         assert.response(app,
             { url: '/' },
             { body: html });
+        assert.response(app,
+            { url: '/none' },
+            { body: '' });
     }
 };
